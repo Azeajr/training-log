4b8201f6b8b8083c35552e102978386da7832429
Antonio Zea <zeajantonio@gmail.com>
2026-05-08 18:47:05 -0400

    Seed DB before first render; show loading placeholder during init

--- src-solid/main.tsx
+++ src-solid/main.tsx
@@ -1,5 +1,9 @@
 import { render } from 'solid-js/web'
 import '../src/index.css'
 import App from './App'
+import { dbReady } from '../src/db/db-v2'
+import { seedDatabase } from '../src/db/seed'
 
-render(() => <App />, document.getElementById('root')!)
+dbReady.then(seedDatabase).then(() => {
+  render(() => <App />, document.getElementById('root')!)
+})
